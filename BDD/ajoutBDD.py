7cc735a700c64fcefebd5e9afafa1708f049d20d
Simon <simon.louis638@gmail.com>
2023-10-20 11:26:25 +0200

    form relier a la bdd

--- BDD/ajoutBDD.py
+++ BDD/ajoutBDD.py
@@ -2,14 +2,16 @@ import sqlite3
 import os
 from flask import Flask, request, render_template, redirect
 
+app = Flask(__name__, template_folder=os.getcwd())
+
 def ajout():
-    con = sqlite3.connect('SdfTown.db')
+    con = sqlite3.connect(os.path.join(os.getcwd(), 'SdfTown.sqlite'))
     cursor = con.cursor()
     cursor.execute("SELECT * FROM départements")
     result = cursor.fetchall()
-    print(result)
+    con.close()
+    return result
 
-app = Flask(__name__, template_folder=os.getcwd())
 @app.route('/savedata', methods=['POST'])
 def savedata():
     prénom = request.form.get('prenom')
@@ -17,12 +19,13 @@ def savedata():
     mdp = request.form.get('mdp')
     email = request.form.get('email')
     tel = request.form.get('tel')
-    con = sqlite3.connect('SdfTown.db')
+    con = sqlite3.connect(os.path.join(os.getcwd(), 'SdfTown.sqlite'))
     cursor = con.cursor()
-    cursor.execute("INSERT INTO clients (prénom, nom, mdp, email, tel) VALUES (?, ?, ?, ?, ?)", (prénom, nom, mdp, email, tel))
+    cursor.execute("INSERT INTO clients ('prénom', 'nom', 'mot de passe', 'email', 'téléphone') VALUES (?, ?, ?, ?, ?)", (prénom, nom, mdp, email, tel))
     con.commit()
     con.close()
     print(prénom, nom, mdp, email, tel)
+    return "Données enregistrées"
 
 if __name__ == "__main__":
     app.run(debug=True)
