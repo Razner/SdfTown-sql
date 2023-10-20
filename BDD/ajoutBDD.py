6af876d7d1d502c54c211de4edeb679d4bbc11b4
Simon <simon.louis638@gmail.com>
2023-10-20 11:03:41 +0200

    relier formulaire a la bdd V1

--- BDD/ajoutBDD.py
+++ BDD/ajoutBDD.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+from flask import Flask, request, render_template, redirect
 
 def ajout():
     con = sqlite3.connect('SdfTown.db')
@@ -7,4 +9,20 @@ def ajout():
     result = cursor.fetchall()
     print(result)
 
-ajout()
+app = Flask(__name__, template_folder=os.getcwd())
+@app.route('/savedata', methods=['POST'])
+def savedata():
+    prénom = request.form.get('prenom')
+    nom = request.form.get('nom')
+    mdp = request.form.get('mdp')
+    email = request.form.get('email')
+    tel = request.form.get('tel')
+    con = sqlite3.connect('SdfTown.db')
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO clients (prénom, nom, mdp, email, tel) VALUES (?, ?, ?, ?, ?)", (prénom, nom, mdp, email, tel))
+    con.commit()
+    con.close()
+    print(prénom, nom, mdp, email, tel)
+
+if __name__ == "__main__":
+    app.run(debug=True)
