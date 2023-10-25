d11df2f6a4c9b2c691a49804470689ffa1484527
fpierrea <pierrealexis.favril@ynov.com>
2023-10-25 10:10:18 +0200

    permet de naviguer entre les pages

--- serveur.py
+++ serveur.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+import webbrowser
 
 port = 80
 adresse = ("", port)
@@ -8,6 +9,6 @@ handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(adresse, handler)
 
 print(f"Server started on port {port}")
-print(f"Server address: http://localhost:{port}")
+print(f"Server address: http://localhost:{port}/HTML/index.html")l)
 
 httpd.serve_forever()
