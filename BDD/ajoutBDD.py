a4213ddb4334da52cee4ef8e66c70baf60b8ca8e
fpierrea <pierrealexis.favril@ynov.com>
2023-10-27 11:59:40 +0200

    mise en place

--- BDD/ajoutBDD.py
+++ BDD/ajoutBDD.py
@@ -4,17 +4,18 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__, template_folder=os.getcwd())
 
-def ajout():
+def select():
     con = sqlite3.connect(os.path.join(os.getcwd(), 'SdfTown.sqlite'))
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM départements")
+    cursor.execute("SELECT * FROM clients")
     result = cursor.fetchall()
+    print(result)
     con.close()
     return result
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('templates/home.html')
 
 @app.route('/savedata', methods=['POST'])
 def savedata():
@@ -29,11 +30,17 @@ def savedata():
     con.commit()
     con.close()
     print(prénom, nom, mdp, email, tel)
-    return "Données enregistrées"
+    return render_template('templates/page2.html')
 
 @app.route('/page2')
 def page2():
+    return (render_template('templates/page2.html'))
+
+@app.route('/connect')
+def connect():
     return render_template('templates/page2.html')
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
